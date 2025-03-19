c227ff11f56686084708bd11025b435345629991
village-of-book <tokyotower0131@yahoo.co.jp>
2025-03-19 22:32:01 +0900

    push01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
                             @if (Route::has('login'))
                                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                                     @auth
-                                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ダッシュボード</a>
+                                        <a href="{{ route('posts.index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">一覧画面</a>
                                     @else
                                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
 
